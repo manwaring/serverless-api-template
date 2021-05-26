62d0840252e323f184e0836a8d6f51144eba846c
Phillip Manwaring <phillip.manwaring@shopify.com>
2021-05-25 21:33:12 -0400

    update dependencies

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }]
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ["@babel/plugin-proposal-private-methods", { "loose": true }]
   ],
   presets: [['@babel/preset-env', { targets: { node: 10 } }], '@babel/preset-typescript']
 };
